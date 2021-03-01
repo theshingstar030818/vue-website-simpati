f864a19929f666e601d9775ca8c801eae2d72bdc
Shen Yuan Zhe <58937356+forwardman1993@users.noreply.github.com>
2021-02-28 17:56:55 -0800

    push existing code

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+
+use Hash;
+use Illuminate\Auth\Events\PasswordReset;
+
+use App\User;
+use Validator;
+
+
+class AuthController extends Controller
+{
+    use SendsPasswordResetEmails;
+
+    /**
+    * Create user
+    *
+    * @param  [string] name
+    * @param  [string] email
+    * @param  [string] password
+    * @param  [string] password_confirmation
+    * @return [string] message
+    */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email'=>'required|string|unique:users',
+            'password'=>'required|string',
+            'c_password' => 'required|same:password'
+        ]);
+
+        $user = new User([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if($user->save()){
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->plainTextToken;
+
+            return response()->json([
+            'message' => 'Successfully created user!',
+            'accessToken'=> $token,
+            ],201);
+        }
+        else{
+            return response()->json(['error'=>'Provide proper details']);
+        }
+    }
+
+    /**
+     * Login user and create token
+    *
+    * @param  [string] email
+    * @param  [string] password
+    * @param  [boolean] remember_me
+    */
+
+    public function login(Request $request)
+    {
+        $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+        'remember_me' => 'boolean'
+        ]);
+
+        $credentials = request(['email','password']);
+        if(!Auth::attempt($credentials))
+        {
+        return response()->json([
+            'message' => 'Unauthorized'
+        ],401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->plainTextToken;
+
+        return response()->json([
+        'accessToken' =>$token,
+        'token_type' => 'Bearer',
+        ]);
+    }
+
+    /**
+     * Get the authenticated User
+    *
+    * @return [json] user object
+    */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+    /**
+     * Logout user (Revoke the token)
+    *
+    * @return [string] message
+    */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+        'message' => 'Successfully logged out'
+        ]);
+
+    }
+
+    /**
+     * Send password reset link. 
+     */
+    public function sendPasswordResetLink(Request $request)
+    {
+        return $this->sendResetLinkEmail($request);
+    }
+
+    /**
+     * Get the response for a successful password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkResponse(Request $request, $response)
+    {
+        return response()->json([
+            'message' => 'Password reset email sent.',
+            'data' => $response
+        ]);
+    }
+    /**
+     * Get the response for a failed password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return response()->json(['message' => 'Email could not be sent to this email address.']);
+    }
+
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        return response()->json(['message' => 'Password reset successfully.']);
+    }
+
+    /**
+     * Get the response for a failed password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return response()->json(['message' => 'Failed, Invalid Token.']);
+    }
+
+
+
+}
